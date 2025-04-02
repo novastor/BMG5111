b4f10291a4394c0ea232acd4013c33495ebd8a15
josh <140547038+novastor@users.noreply.github.com>
2025-04-01 21:38:03 -0400

    Update realtime_whisper.py

--- utils/realtime_whisper.py
+++ utils/realtime_whisper.py
@@ -26,7 +26,11 @@ import os
 #    #wav_buffer = convert_bytearray_to_wav_ndarray(wav_buffer)
 #    return wav_buffer
 def audio_mockup():
-    return "the patient suffered an acute stroke with no further complications"
+    def audio_mockup():
+    mockup = io.BytesIO()
+    mockup.write("the patient suffered an acute stroke with no further complications")
+    mockup.seek(0)
+    return mockup
 def audio_processing():
         """
         audio file processing and text generation function 
